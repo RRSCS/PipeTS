b4c9d32e0371dc122ce8a25ca9e5bed9dc50ef1d
Magnus Reeves <rreeves8@uwo.ca>
2023-09-29 23:25:07 -0400

    add globals

--- globals.d.ts
+++ globals.d.ts
@@ -1,16 +1,42 @@
 type PipelineNode = {
+  objectID: string;
   cmd: Array<string>;
+  logs: Array<string>;
+  status: "failed" | "completed" | "dead" | "active";
 };
 
-type Pipeline = PipelineNode | Array<Pipeline>;
+type PipelineNodes = PipelineNode | Array<PipelineNodes>;
 
 type EnvVar = {
   name: string;
   value: unknown;
 };
 
-type SavedPipeline = {
+type Pipeline = {
   envVars: Array<EnvVar>;
-  cliDependencies: Array<string>;
-  pipeline: Array<Pipeline>;
+  clis: Array<string>;
+  nodes: Array<PipelineNodes>;
 };
+
+type StoredPipelines = Array<Pipeline>;
+
+type StoredPipelineRef = string;
+
+type ActivePipelines = Array<StoredPipelineRef>;
+type PreviousPipelines = Array<StoredPipelineRef>;
+
+type NodeRef = string;
+
+//from ui cluster to cmd cluster to boot pipeline
+type NewPipelinePayload = StoredPipelineRef;
+
+//from ui cluster back to ui, on init
+type NetworkPayload =
+  | {
+      type: "state-change";
+      activeNodes: Array<PipelineNode>;
+    }
+  | {
+      type: "log-strean";
+      logs: Array<{ node: NodeRef; data: string }>;
+    };
